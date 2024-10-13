994484b36441fee4709e21906367d6de3a0829b1
SerhiiBruj <sergijk174@gmail.com>
2024-10-13 20:02:22 +0300

    Updates

---
+++ assets/circle-Bavmbehg.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-D0FHdcGl.js";const e=i=>o.jsx("div",{style:{backgroundColor:i.color,height:i.size,width:i.size,borderRadius:"50%",position:"relative",top:0}});export{e as C};
