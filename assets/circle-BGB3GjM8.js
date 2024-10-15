3148dc1163acfc70e0d71615d3fb96dfc7f0cd87
SerhiiBruj <sergijk174@gmail.com>
2024-10-15 20:01:09 +0300

    Updates

---
+++ assets/circle-BGB3GjM8.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-D6RTl2c-.js";const e=i=>o.jsx("div",{style:{backgroundColor:i.color,height:i.size,width:i.size,borderRadius:"50%",position:"relative",top:0}});export{e as C};
